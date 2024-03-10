53e3e071840f250b0df4b2e7f8f580276f322e80
Fabs <fabiolacastanedamondragon2004@gmail.com>
2024-03-09 18:26:32 -0600

    first commit

---
+++ ejercicio3.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main(){
+    double var1=40;
+    double var2=20;
+
+
+      int res=var1/var2;
+
+    printf("El resultado de la division es: %d",res);
+
+
+printf("\n");
+return 0;
+}
