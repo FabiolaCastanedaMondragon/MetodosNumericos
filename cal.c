c7880ba5cec4794d75525d4c2f1dd7ec771544ab
Fabs <fabiolacastanedamondragon2004@gmail.com>
2024-05-06 20:45:48 -0600

    first commit

---
+++ cal.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdbool.h>
+
+int main(){
+    int calf;
+    scanf("%d", &calf);
+    if(calf >=8){
+    printf (" Felicidades aprovaste el curso\n");
+    
+    }else
+    printf (" No aprovaste el curso\n");
+    
+    return 0;
+}
