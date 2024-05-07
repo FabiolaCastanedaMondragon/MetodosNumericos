853cf2e258e595e6187c423f85dbd7a5583d7f92
Fabs <fabiolacastanedamondragon2004@gmail.com>
2024-05-06 20:44:36 -0600

    first commit

---
+++ j.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main () {
+    int min=60;
+    int hora=60;
+    int dia=24;
+   int j=19;
+    
+    printf("%d ", j*min*hora*dia*365);
+
+    printf("\n");
+    return 0;
+}
