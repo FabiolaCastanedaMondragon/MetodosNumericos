3985940c636e31d416858b4bea7594c41bb5d0d9
Fabs <fabiolacastanedamondragon2004@gmail.com>
2024-05-07 21:21:00 -0600

    first commit

---
+++ holaMundo.c
@@ -0,0 +1,8 @@
+#include <stdio.h> //Bibliotecas (library) studio--standar input-output h--> Permite arrastrar datos a la pantalla
+#include <stdlib.h>
+
+int main (){ //funcion principal
+    printf ("Hello word"); // el punto y coma indica que se llavara lo que esta antes del ; a la memoria RAM
+    system("Pause");
+    return 0;
+}
